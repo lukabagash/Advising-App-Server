6f13373f1b373e0a892e6b7bdee629f29c527f9b
Luka <luka.yep@gmail.com>
2023-06-04 21:15:36 -0400

    adding local repository to github

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,15 @@
 export const sshConfig = {
-    host: 'jakku.cs.denison.edu',
-    port: 22,
-    username: 'bagash_l2',
-    password: 'Micronokia123!'
-  };
-  
-  export const dbConfig = {
-    host: 'jakku.cs.denison.edu',
-    port: 3306,
-    user: 'bagash_l2',
-    database: 'app_schedule',
-    password: 'Micronokia123!'
-  };
+  host: 'jakku.cs.denison.edu',
+  port: 22,
+  username: 'bagash_l2',
+  password: process.env.ADVISING_APP_CONFIG
+};
+
+export const dbConfig = {
+  host: 'jakku.cs.denison.edu',
+  port: 3306,
+  user: 'bagash_l2',
+  database: 'app_schedule',
+  password: process.env.ADVISING_APP_CONFIG
+};
   
