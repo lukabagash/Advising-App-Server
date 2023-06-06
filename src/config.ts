46b94e7743443a1cc7da550574c0a9959b0ae9dd
Luka <luka.yep@gmail.com>
2023-06-06 12:09:06 -0400

    files synchronized with server and running

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,8 @@
-export const sshConfig = {
-  host: 'jakku.cs.denison.edu',
-  port: 22,
-  username: 'bagash_l2',
-  password: process.env.ADVISING_APP_CONFIG
-};
-
 export const dbConfig = {
   host: 'jakku.cs.denison.edu',
   port: 3306,
   user: 'bagash_l2',
   database: 'app_schedule',
-  password: process.env.ADVISING_APP_CONFIG
+  password: 'Micronokia123!'
 };
   
